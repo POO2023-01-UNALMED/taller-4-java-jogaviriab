07e186e7c6a804e830f6cadf1a52222d8a3cbe27
jogaviriab <jogaviriab@unal.edu.co>
2023-03-05 14:11:45 -0500

    2

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -8,9 +8,14 @@ public class Persona {
     
     static {
         totalPersonas = 0;
-        cedula = 3;
+        
     }
 
+    public Persona() {
+    	this.nombre = "";
+    	this.cedula = 0;
+    }
+    
     public Persona(long cedula, String nombre) {
         this.cedula = cedula;
         this.nombre = nombre;
@@ -31,6 +36,7 @@ public class Persona {
 
     public Persona(String nombre) {
         this.nombre = "";
+        cedula = 1;
         totalPersonas++;
     }
     
